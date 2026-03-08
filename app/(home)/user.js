bb044989c3a5439a4b12ff2c95db8ac64ac77eec
EzeGomez0407 <gomezmerloe@gmail.com>
2026-03-07 23:21:42 -0300

    agregue tailwindcss para facilitar algunos estilos

--- app/(home)/user.js
+++ app/(home)/user.js
@@ -1,7 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function User(){
-    return <View>
-        <Text>Ruta Usuario</Text>
+    return <View className="mt-20">
+        <Link href={"/"}>
+            <Button mode="container" buttonColor="green" textColor="white">{"<"}Atras</Button>
+        </Link>
+        <Text className="mt-10">Ruta Usuario</Text>
     </View>
 }
