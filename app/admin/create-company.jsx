e752336f1268ef03a7267651e6c9747a5cbfa0c7
EzeGomez0407 <gomezmerloe@gmail.com>
2026-03-20 17:18:53 -0300

    envolvi el formulario de crear empresa con el componente KeyboardLayout por prevención

--- app/admin/create-company.jsx
+++ app/admin/create-company.jsx
@@ -7,6 +7,7 @@ import BasicInputs from "../../components/to-forms/BasicInputs";
 import BasicMsgError from "../../components/to-forms/BasicMsgError";
 import { Button, Surface } from "react-native-paper";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import KeyboardLayout from "../../components/layout/KeyboardLayout";
 
 export default function CreateCompany() {
   return (
@@ -36,53 +37,59 @@ export default function CreateCompany() {
           ),
         }}
       />
-      <View className="py-10">
-        <BasicInputs
-          label={" Nombre de la empresa"}
-          placeholder={"Ingresá el nombre de tu empresa"}
-          leftIcon={
-            <MaterialCommunityIcons
-              name="office-building-plus-outline"
-              size={20}
-              color="#FFD700"
-            />
-          }
-          style={{ fontSize: 18 }}
-        />
-        <BasicMsgError />
-        <BasicInputs
-          label={" Dirección"}
-          placeholder={"Ingresá el nombre de tu empresa"}
-          leftIcon={
-            <SimpleLineIcons name="location-pin" size={20} color="#FFD700" />
-          }
-          style={{ fontSize: 18 }}
-        />
-      </View>
-      <View className="flex-row bg-[#4477ff]/10 p-5 rounded-2xl border border-[#E7D77B]/40 mb-10">
-        <Text className="text-lg text-[#405C4D]">
-          <Text className="text-[#003366]">💡Importante </Text>
-          Estos datos serán visibles para tus empleados y servirán para
-          identificar tu empresa dentro de la aplicación.
-        </Text>
-      </View>
+      <KeyboardLayout>
+        <View className="py-10">
+          <BasicInputs
+            label={" Nombre de la empresa"}
+            placeholder={"Ingresá el nombre de tu empresa"}
+            leftIcon={
+              <MaterialCommunityIcons
+                name="office-building-plus-outline"
+                size={20}
+                color="#FFD700"
+              />
+            }
+            style={{ fontSize: 18 }}
+          />
+          <BasicMsgError />
+          <BasicInputs
+            label={" Dirección"}
+            placeholder={"Ingresá el nombre de tu empresa"}
+            leftIcon={
+              <SimpleLineIcons name="location-pin" size={20} color="#FFD700" />
+            }
+            style={{ fontSize: 18 }}
+          />
+        </View>
+        <View className="flex-row bg-[#4477ff]/10 p-5 rounded-2xl border border-[#E7D77B]/40 mb-10">
+          <Text className="text-lg text-[#405C4D]">
+            <Text className="text-[#003366]">💡Importante </Text>
+            Estos datos serán visibles para tus empleados y servirán para
+            identificar tu empresa dentro de la aplicación.
+          </Text>
+        </View>
 
-      <Surface style={{ borderRadius: 15, shadowColor: "#c7b75b" }}>
-        <Button
-          onPress={() => router.navigate("/admin/works-view")}
-          mode="contained"
-          icon={() => (
-            <MaterialIcons name="arrow-forward-ios" size={16} color="#225599" />
-          )}
-          buttonColor="#FFD700"
-          textColor="#225599"
-          labelStyle={{ fontSize: 16 }}
-          contentStyle={{ height: 50, flexDirection: "row-reverse" }}
-          style={{ borderRadius: 15 }}
-        >
-          Registrar Empresa
-        </Button>
-      </Surface>
+        <Surface style={{ borderRadius: 15, shadowColor: "#c7b75b" }}>
+          <Button
+            onPress={() => router.navigate("/admin/works-view")}
+            mode="contained"
+            icon={() => (
+              <MaterialIcons
+                name="arrow-forward-ios"
+                size={16}
+                color="#225599"
+              />
+            )}
+            buttonColor="#FFD700"
+            textColor="#225599"
+            labelStyle={{ fontSize: 16 }}
+            contentStyle={{ height: 50, flexDirection: "row-reverse" }}
+            style={{ borderRadius: 15 }}
+          >
+            Registrar Empresa
+          </Button>
+        </Surface>
+      </KeyboardLayout>
     </ScreenLayout>
   );
 }
