2c74fc7b1a40b0b883e3e22fcda11a6ca0cad91c
EzeGomez0407 <gomezmerloe@gmail.com>
2026-03-26 17:50:20 -0300

    boton + input de busqueda de herramientas agregados en pagina de vista de herramientas

--- app/admin/(tools)/tools-view.jsx
+++ app/admin/(tools)/tools-view.jsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 import ScreenLayout from "../../../components/layout/ScreenLayout";
 import { Text, View } from "react-native";
 import { Icons } from "../../../assets/icons";
+import { Button, Surface } from "react-native-paper";
+import BasicInputs from "../../../components/to-forms/BasicInputs";
 
 export default function ToolsView() {
   return (
@@ -26,6 +28,29 @@ export default function ToolsView() {
           ),
         }}
       />
+      <View className="w-full items-center gap-4">
+        <Surface style={{ borderRadius: 15 }} className="mt-8 w-full">
+          <Button
+            onPress={() => {}}
+            mode="contained"
+            icon={() => <Icons.plus size={20} color="#225599" />}
+            buttonColor="#FFD700"
+            textColor="#225599"
+            labelStyle={{ fontSize: 18 }}
+            contentStyle={{ height: 50 }}
+            style={{ borderRadius: 15 }}
+          >
+            Agregar herramienta
+          </Button>
+        </Surface>
+
+        <BasicInputs
+          label={"Buscar herramientas..."}
+          placeholder={"Matesito"}
+          leftIcon={<Icons.search size="18" color="#225599" />}
+          style={{ width: "100%" }}
+        />
+      </View>
     </ScreenLayout>
   );
 }
